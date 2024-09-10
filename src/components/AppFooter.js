e14dd50cf8df8d010ec68b03a695bcb14d42925c
Adrian <lopez120696@gmail.com>
2024-09-10 16:59:38 -0600

    dashboard limpio

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,16 +5,16 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
+        {/* <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
           CoreUI
-        </a>
-        <span className="ms-1">&copy; 2024 creativeLabs.</span>
+        </a> */}
+        <span className="ms-1">&copy; 2024 OptiMaster</span>
       </div>
       <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
+        {/* <span className="me-1">Powered by</span> */}
+        {/* <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
           CoreUI React Admin &amp; Dashboard Template
-        </a>
+        </a> */}
       </div>
     </CFooter>
   )
--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -24,7 +24,7 @@ import CIcon from '@coreui/icons-react'
 import { signOut } from 'firebase/auth';
 import { auth } from '../../config/firebaseConfig';
 import { useNavigate } from 'react-router-dom';
-import avatar8 from './../../assets/images/avatars/8.jpg'
+import avatar6 from './../../assets/images/avatars/6.jpg'
 
 const AppHeaderDropdown = () => {
   const navigate = useNavigate();
@@ -41,7 +41,7 @@ const AppHeaderDropdown = () => {
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
+        <CAvatar src={avatar6} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
